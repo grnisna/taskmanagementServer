b04eb0db72f3246255f52d6fe2e870a7d23e12a9
grnisan <mdnisanahmed63@gmail.com>
2022-07-02 00:15:15 +0600

    mongodb added

--- index.js
+++ index.js
@@ -1,21 +1,46 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port =  process.env.PORT || 5000;
 
+// milldle ware 
+app.use(cors());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
 const uri = "mongodb+srv://todotask:KFhmghf31cuXL03h@cluster0.7oiep.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('wow running');
-  client.close();
-});
+
+
+async function run(){
+    try{
+        await client.connect();
+        const todoCollection = client.db("taskmanagement").collection("todotask");
+        
+        
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = todoCollection.find(query);
+
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+    }
+    finally{
+        // client.close();
+    }
+
+}
+run().catch(console.dir)
+
+
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('The Server is working')
   })
   
   app.listen(port, () => {
-    console.log(`Running Port is -- ${port}`)
+    console.log(`Running Port is --${port}`)
   })
